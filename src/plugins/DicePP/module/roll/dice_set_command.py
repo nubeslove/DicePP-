6de744638a771a1b29e3e7c2e7a6d1d91479e6d2
SilverDragon <kagangtuya@foxmail.com>
2025-09-24 17:26:44 +0800

    fix：。dset命令触发的回复异常
    在 src/plugins/DicePP/module/roll/dice_set_command.py:53 起补充了兼容处理：无论使用新占位符 {expr} 还是旧的 {dice}，都会同时提供参数；对失败提示也补回 min_face / max_face 默认值，避免旧格式再次报错。

--- src/plugins/DicePP/module/roll/dice_set_command.py
+++ src/plugins/DicePP/module/roll/dice_set_command.py
@@ -10,6 +10,7 @@ from module.roll.default_dice import (
     format_default_expr_from_input,
     format_default_expr_from_storage,
 )
+from module.roll.roll_config import DICE_TYPE_MAX
 from module.roll.roll_utils import RollDiceError
 
 LOC_DSET_SUCCESS = "roll_default_dice_set_success"
@@ -55,17 +56,20 @@ class DiceSetCommand(UserCommandBase):
         current_expr = format_default_expr_from_storage(stored)
 
         if not arg:
-            feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_CURRENT, expr=current_expr)
+            feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_CURRENT, expr=current_expr, dice=current_expr)
             return [BotSendMsgCommand(self.bot.account, feedback, [port])]
 
         try:
             new_expr = format_default_expr_from_input(arg)
         except RollDiceError as exc:
-            feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_INVALID, reason=exc.info)
+            feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_INVALID,
+                                                          reason=exc.info,
+                                                          min_face="2",
+                                                          max_face=str(DICE_TYPE_MAX))
             return [BotSendMsgCommand(self.bot.account, feedback, [port])]
 
         self.bot.data_manager.set_data(DC_GROUPCONFIG, [meta.group_id, "default_dice"], new_expr)
-        feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_SUCCESS, expr=new_expr)
+        feedback = self.bot.loc_helper.format_loc_text(LOC_DSET_SUCCESS, expr=new_expr, dice=new_expr)
         return [BotSendMsgCommand(self.bot.account, feedback, [port])]
 
     def get_help(self, keyword: str, meta: MessageMetaData) -> str:
